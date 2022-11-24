db1619251632fc178b57ccc653cff46dc589c203
GustavoJST <gutojusto22@gmail.com>
2022-11-23 23:15:06 -0300

    Updated projects and added favicon.

---
+++ app.js
@@ -0,0 +1,16 @@
+var project1 = document.getElementById("project1")
+let project2 = document.getElementById("project2")
+let project3 = document.getElementById("project3")
+
+project1.addEventListener("click", function () {
+    window.location.href = "https://github.com/GustavoJST/CLID";
+});
+
+project2.addEventListener("click", function () {
+    window.location.href = "https://github.com/GustavoJST/Flask-Finance-Website";
+});
+
+project3.addEventListener("click", function () {
+    window.location.href = "https://github.com/GustavoJST/Calculator";
+});
+
